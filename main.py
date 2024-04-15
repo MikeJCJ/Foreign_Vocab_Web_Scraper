8c2a48f9ecf408351bd1c58d9592e216d31ae45b
MikeJCJ <mikeyjohnson1@live.co.uk>
2024-04-15 21:00:13 +0100

    added in time delay

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import FScrape
 from Article import Article
-import numpy
+import numpy as np
 import json
+import time
 
 # INPUTS
 nos_latest_news_url = "https://nos.nl/nieuws/laatste"
+mu, sigma = 5,2 # Scraper delay variables
 
-if __name__=="__main__":
+def webDataScrape():
     # Get list of links from NOS latest news page
     nos_news_soup, nos_news_html = FScrape.soupTextFromURL(nos_latest_news_url)
     #writeStringToFile(nos_news_html, "output.txt")
@@ -16,6 +18,10 @@ if __name__=="__main__":
     article_list = []
     for link in link_list[0:1]: # TODO REMOVE SLICER
         article_list.append(Article(link))
+        delay = np.random.normal(mu, sigma)
+        if delay<2: delay=2
+        elif delay>10: delay=10
+        time.sleep(delay)
     
     for article in article_list:
         print(f"link: {article.url}")
@@ -23,3 +29,5 @@ if __name__=="__main__":
         print(F"\narticle_text: {article.text}")
         print(F"\narticle_identifier: {article.ID}")
         print(F"\narticle_date_time: {article.date_time}")
+
+webDataScrape()
