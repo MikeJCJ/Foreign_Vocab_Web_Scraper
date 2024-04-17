f4a93f90d9625cbbda8607bce089d3fc23bb8e96
MikeJCJ <mikeyjohnson1@live.co.uk>
2024-04-17 21:21:01 +0100

    updateMasterList refactoring

--- webScraper.py
+++ webScraper.py
@@ -16,7 +16,7 @@ def webDataScrape():
     
     # Create Aricle objects
     article_list = []
-    for link in link_list:
+    for link in link_list[0:5]:
         article_list.append(Article.fromScrape(link))
         delay = np.random.normal(mu, sigma)
         if delay<1: delay=1
@@ -27,5 +27,39 @@ def webDataScrape():
     json_string = json.dumps(article_dict_list)
     with open('articles_scrapped.json','w') as outfile:
         outfile.write(json_string)
+
+def updateMasterList():
+    master_article_list = []
+    try:
+        with open('master_article_list.json') as json_file:
+            master_article_data = json.load(json_file)
+            for article in master_article_data:
+                master_article_list.append(Article(article['ID'], article['url'], article['title'], article['date_time'], article['text']))
+    except:
+        print("There is no existing Master Article list")
     
-    return article_dict_list
+    # Retrieve list of scrapped articles and convert into Article objects
+    scrapped_article_list = []
+    try:
+        with open('articles_scrapped.json') as json_file:
+            scrapped_article_data = json.load(json_file)
+            for article in scrapped_article_data:
+                scrapped_article_list.append(Article(article['ID'], article['url'], article['title'], article['date_time'], article['text']))
+    except:
+        print("There is no existing Scrapped Article list")
+    
+    # See if newly scrapped articles exist already in master article list, if not then add them
+    match_found=0
+    for scrapped_article in scrapped_article_list:
+        for master_article in master_article_list:
+            if scrapped_article.ID == master_article.ID:
+                match_found=1
+                break
+        if match_found == 0:
+            master_article_list.append(scrapped_article)
+        elif match_found == 1:
+            match_found = 0
+    article_dict_list = [{"ID":article.ID, "url":article.url, "title":article.title, "date_time":article.date_time, "text":article.text} for article in master_article_list]
+    json_string = json.dumps(article_dict_list)
+    with open('master_article_list.json','w') as outfile:
+        outfile.write(json_string)
