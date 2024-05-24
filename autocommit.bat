5da1f07cfb033ce704d8209cc74a89e7e886645f
MikeJCJ <mikeyjohnson1@live.co.uk>
2024-05-24 23:30:52 +0100

    daily run: 20240524.233052

---
+++ autocommit.bat
@@ -0,0 +1,7 @@
+cd C:\Users\mikey\OneDrive\OLD\Documents\Coding projects\Foreign_Vocab_Web_Scraper
+
+git add .
+
+git commit -m "daily run: %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+
+git push origin "main"
