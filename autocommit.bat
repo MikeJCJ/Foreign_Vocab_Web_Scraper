96fa94598cb0abd7e4443ef45eb9cdb2d7f5da30
MikeJCJ <mikeyjohnson1@live.co.uk>
2024-05-24 23:51:58 +0100

    TODO update

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,8 @@
+: : Run Jupyter notebook
+cd
+call C:\Users\mikey\AppData\Local\Continuum\anaconda3\Scripts\activate.bat environment_name
+: : TODO NEED TO FIND LOCATION OF CONDA ENV
+
 cd C:\Users\mikey\OneDrive\OLD\Documents\Coding projects\Foreign_Vocab_Web_Scraper
 
 git add .
